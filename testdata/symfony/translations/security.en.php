57a78bd178e2a391419a37d5254642fefce11a6e
Ruben R <ruben@opensoutheners.com>
2026-06-11 21:27:40 +0200

    feat(symfony): add translation key resolver with YAML, PHP, and XLIFF support

---
+++ testdata/symfony/translations/security.en.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'login' => [
+        'title' => 'Login',
+        'username' => 'Username',
+        'password' => 'Password',
+    ],
+    'logout' => 'Logout',
+    'access_denied' => 'Access Denied',
+    'remember_me' => 'Remember Me',
+];
